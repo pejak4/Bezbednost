53a84bbca54a1cbec52bbc6e903a0428c9f89aff
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-05-26 14:46:26 +0200

    Add headers on axios call
    Authorization with bearer + token

--- kt2/ktbackend/src/main/java/com/example/demo/service/UserService.java
+++ kt2/ktbackend/src/main/java/com/example/demo/service/UserService.java
@@ -62,7 +62,7 @@ public class UserService {
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8");
         dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("JovanJenjic123");
+        dataSourceBuilder.password("retturn.05be");
 
 
         DataSource dataSource = dataSourceBuilder.build();
@@ -100,7 +100,7 @@ public class UserService {
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8");
         dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("JovanJenjic123");
+        dataSourceBuilder.password("retturn.05be");
 
 
         DataSource dataSource = dataSourceBuilder.build();
